2f9fd6c1e89da57ab5f492608171cf77aed40940
kurt-caves <kurtalyell@gmail.com>
2023-04-05 12:19:38 -0600

    added arch diagram

--- Assets/LobbyUI.cs
+++ Assets/LobbyUI.cs
@@ -1,3 +1,77 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2ed4af61d76a0999edb5d4b340a4dcb998bcf771d5b3a3403980e573d2269ebe
-size 1976
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Services.Authentication;
+using Unity.Services.Lobbies.Models;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyUI : MonoBehaviour {
+
+
+    public static LobbyUI Instance { get; private set; }
+
+
+    
+    //[SerializeField] private Transform container;
+    //[SerializeField] private TextMeshProUGUI lobbyNameText;
+    [SerializeField] private TextMeshProUGUI playerCountText;
+    [SerializeField] private TextMeshProUGUI lobbyNameText;
+    [SerializeField] private Button leaveButton;
+    [SerializeField] private Button StartButton;
+    
+
+
+    private void Awake() {
+        Instance = this;
+
+      //  playerSingleTemplate.gameObject.SetActive(false);
+
+        leaveButton.onClick.AddListener(() => {
+            TestLobby.Instance.LeaveLobby();
+        });
+
+        StartButton.onClick.AddListener(() => {
+            TestLobby.Instance.StartGame();
+        });
+    }
+
+    private void Start() {
+        TestLobby.Instance.OnJoinedLobby += UpdateLobby_Event;
+        TestLobby.Instance.OnJoinedLobbyUpdate += UpdateLobby_Event;
+        //TestLobby.Instance.OnLeftLobby += TestLobby_OnLeftLobby;
+      //  TestLobby.Instance.OnKickedFromLobby += TestLobby_OnLeftLobby;
+
+        Hide();
+    }
+
+    private void LobbyManager_OnLeftLobby(object sender, System.EventArgs e) {
+        //ClearLobby();
+        Hide();
+    }
+
+    private void UpdateLobby_Event(object sender, TestLobby.LobbyEventArgs e) {
+        UpdateLobby();
+    }
+
+    private void UpdateLobby() {
+        UpdateLobby(TestLobby.Instance.GetJoinedLobby());
+    }
+
+    private void UpdateLobby(Lobby lobby) {
+    
+        lobbyNameText.text = "Lobby Name: " + lobby.Name;
+        playerCountText.text = "Number of players: " + lobby.Players.Count;
+       
+        Show();
+    }
+
+    private void Hide() {
+        gameObject.SetActive(false);
+    }
+
+    public void Show() {
+        gameObject.SetActive(true);
+    }
+
+}
--- Assets/RelayManager.cs
+++ Assets/RelayManager.cs
@@ -1,3 +1,64 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:72fba007ae2fc0c882e024599dcde91d81619eb1c58bee810f54e4e90938a595
-size 1846
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using Unity.Networking.Transport.Relay;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine;
+using Unity.Netcode;
+using System.Threading.Tasks;
+
+public class RelayManager : MonoBehaviour
+{
+    
+    public static RelayManager Instance { get; private set; }
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    public async Task<string> CreateRelay() {
+
+        try{
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(3); //Create allocation. host + 3 players 
+
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId); //Get join code
+
+            Debug.Log(joinCode);
+
+            RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+            
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+            NetworkManager.Singleton.StartHost();
+
+            return joinCode;
+        } catch(RelayServiceException e) {
+            Debug.Log(e);
+            return null;
+        }
+       
+    }
+
+    public async void JoinRelay(string joinCode) {
+        try{
+            Debug.Log("Joining relay with "+ joinCode);
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+             RelayServerData relayServerData = new RelayServerData(joinAllocation, "dtls");
+            
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+            NetworkManager.Singleton.StartClient();
+
+        } catch (RelayServiceException e){
+            Debug.Log(e);
+        }
+        
+    }
+    
+
+   
+}
