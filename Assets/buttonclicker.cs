fc9a8ad1336e775c4a8c961b546652253131d2cb
kurt-caves <kurtalyell@gmail.com>
2023-04-19 13:18:24 -0600

    abandoning this banch

---
+++ Assets/buttonclicker.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonclicker : MonoBehaviour
+{
+    // UIDocument buttonDocument;
+    // buttonclicker uiButton;
+    // void OnEnable(){
+    //     buttonDocument = GetComponent<UIDocument>();
+    //     if(buttonDocument == null){
+    //         Debug.LogError("No UIDocument found on this game object");
+    //     }
+    //     // button root query
+    //     uiButton = buttonDocument.rootVisualElement.Q<buttonclicker>();
+    // }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
