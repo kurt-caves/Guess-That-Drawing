1014ec9cf4f906cbe403336454de4161643090d9
buddyhannah <hannahhimelright@gmail.com>
2023-05-09 15:41:12 -0600

    Fixed issue with changing chat box text color

--- Assets/Canvas/CanvasTools.cs
+++ Assets/Canvas/CanvasTools.cs
@@ -9,24 +9,19 @@ public class CanvasTools : MonoBehaviour
     public static CanvasTools Instance { get; private set; }
 
 
-    //added
     [SerializeField] private Texture2D colorsTexture;
 
 
     [SerializeField] private Image selectedColorImage;
-   // [SerializeField] private Image selectedColor1Image;
-   // private String colorButton="SelectedColor";
+    [SerializeField] private Image  toolBoxImage;
+   
  
     [SerializeField] private Button BucketFill;
     [SerializeField] private Button Pen;
     [SerializeField] private Button Eraser;
     [SerializeField] private Button PenSquare;
     [SerializeField] private Button PenCircle;
-    //[SerializeField] private Button undo;
-    //[SerializeField] private Button redo;
-   // [SerializeField] private Button Picker;
-   // [SerializeField] private Button SelectedColor;
-    //[SerializeField] private Button SelectedColor1;
+    
 
     [SerializeField] private Slider penBox;
 
@@ -43,42 +38,10 @@ public class CanvasTools : MonoBehaviour
         
         Instance = this;
 
+        penBox.minValue = 1;
         penBox.maxValue = 9;
 
-
-        // SelectedColor.onClick.AddListener(() => {
-        //     colorButton="SelectedColor";
-        //     PixelArtDrawingSystem.Instance.ChangeColorUV(0);
-
-        // });
-        // SelectedColor1.onClick.AddListener(() => {
-        //     colorButton="SelectedColor1";
-        //     PixelArtDrawingSystem.Instance.ChangeColorUV(1);
-        // });
-
-        // if(colorButton=="SelectedColor"){
-        //     if(transform.Find(colorButton).GetComponent<Image>()!=null)
-        //      selectedColorImage = transform.Find(colorButton).GetComponent<Image>();
-        // }
-        // else if(colorButton=="SelectedColor1"){
-        //     if(transform.Find(colorButton).GetComponent<Image>()!=null)
-        //      selectedColor1Image = transform.Find(colorButton).GetComponent<Image>();
-        // }
-
-     
-       // CanvasTools.Instance.DisableButton("undo");
-        //CanvasTools.Instance.DisableButton("Pen");
-        //CanvasTools.Instance.DisableButton("Circle");
-
-        
-        // redo.onClick.AddListener(() => {
-        //     PixelArtDrawingSystem.Instance.pushHistory();
-        // });
-        // undo.onClick.AddListener(() => {
-        //     PixelArtDrawingSystem.Instance.pullHistory();
-        // });
-
-        
+        penBox.value = 4;
 
       
 
@@ -121,7 +84,7 @@ public class CanvasTools : MonoBehaviour
 
        
       
-
+        DisableButton();
     }
     private void Update() {
         
@@ -132,12 +95,7 @@ public class CanvasTools : MonoBehaviour
     }
 
     public void DisableButton (string button) {
-        // if(button == "undo"){
-        //     undo.interactable = false;
-        // }
-        // if(button == "redo"){
-        //     redo.interactable = false;
-        // }
+       
         if(button == "Square"){
             PenSquare.interactable = false;
         }
@@ -159,12 +117,7 @@ public class CanvasTools : MonoBehaviour
        
     }
     public void EnableButton (string button) {
-        // if(button == "undo"){
-        //     undo.interactable = true;
-        // } 
-        // if(button == "redo"){
-        //     redo.interactable = true;
-        // }
+     
         if(button == "Square"){
             PenSquare.interactable = true;
         }
@@ -211,41 +164,29 @@ public class CanvasTools : MonoBehaviour
             Vector2 pixelCoordinates = PixelArtDrawingSystem.Instance.GetColorUV();
             pixelCoordinates.x *= colorsTexture.width;
             pixelCoordinates.y *= colorsTexture.height;
-            /*
-            if(colorButton=="SelectedColor"){
-                    selectedColorImage.color = colorsTexture.GetPixel((int)pixelCoordinates.x, (int)pixelCoordinates.y);
-                    
-            }
-            else if(colorButton=="SelectedColor1"){
-                selectedColor1Image.color = colorsTexture.GetPixel((int)pixelCoordinates.x, (int)pixelCoordinates.y);
-                
-            }
-            */
+          
              selectedColorImage.color = colorsTexture.GetPixel((int)pixelCoordinates.x, (int)pixelCoordinates.y);
         }
     private void EnableButton(){
-         selectedColorImage.enabled = true;
+        selectedColorImage.enabled = true;
         BucketFill.gameObject.SetActive(true);
         Pen.gameObject.SetActive(true);
         Eraser.gameObject.SetActive(true);
         PenSquare.gameObject.SetActive(true);
         PenCircle.gameObject.SetActive(true);
+        toolBoxImage.enabled = true;
 
-        //SelectedColor.gameObject.SetActive(true);
-        //SelectedColor1.gameObject.SetActive(true);
         penBox.gameObject.SetActive(true);
     }
     private void DisableButton(){
-         selectedColorImage.enabled = false;
+        selectedColorImage.enabled = false;
         BucketFill.gameObject.SetActive(false);
         Pen.gameObject.SetActive(false);
         Eraser.gameObject.SetActive(false);
         PenSquare.gameObject.SetActive(false);
         PenCircle.gameObject.SetActive(false);
-        //Picker.gameObject.SetActive(false);
-        //SelectedColor.gameObject.SetActive(false);
-        //SelectedColor1.gameObject.SetActive(false);
         penBox.gameObject.SetActive(false);
+        toolBoxImage.enabled = false;
     }
     
     
